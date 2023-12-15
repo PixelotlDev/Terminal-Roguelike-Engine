b894bc37489de370170d94a5decddf28352d7f4e
PixelotlDev <emmastitt@smallcode.dev>
2023-12-15 17:00:31 +1100

    Begun porting, nothing substantial yet

--- source/Terminal Roguelike Engine.cpp
+++ source/Terminal Roguelike Engine.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#if defined(__linux__)
+    #define OS_Linux
+#elif defined(_WIN32)
+    #define OS_Windows
+    #include <windows.h>
+#endif
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // set fullscreen
+    SetConsoleDisplayMode(hStdOut, CONSOLE_FULLSCREEN_MODE, 0);
+
+    COORD bufferSize = { 126, 33 };
+    SetConsoleScreenBufferSize(hStdOut, bufferSize);
 }
